a7ca1dd87112771f93db7e8de6995ff6a3441532
Taylor Smith <taylorsmith@Taylors-MacBook-Pro-2.local>
2022-05-20 14:18:31 -0700

    v0.5 for realsies

--- dist/index.js
+++ dist/index.js
@@ -29958,16 +29958,7 @@ const updateFunctions = __nccwpck_require__(8413);
 
 async function run() {
   try {
-    // const ms = core.getInput('milliseconds');
-    // core.info(`Waiting ${ms} milliseconds ...`);
-
-    // core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    // await wait(parseInt(ms));
-    // core.info((new Date()).toTimeString());
-
-    // core.setOutput('time', new Date().toTimeString());
-    // const stackname = core.getInput('stackname');
-    const stackname = 'govyrl/shopify-fulfillment-pipeline'
+    const stackname = core.getInput('stackname');
 
     if(!stackname) {
         throw Error('stackname must be defined!')
