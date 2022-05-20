4a5ee4e8c373f7f816827bebda97e4dae5210aed
Taylor Smith <taylorsmith@Taylors-MacBook-Pro-2.local>
2022-05-20 14:17:52 -0700

    fix overrides

--- index.js
+++ index.js
@@ -3,16 +3,7 @@ const updateFunctions = require('./update-function');
 
 async function run() {
   try {
-    // const ms = core.getInput('milliseconds');
-    // core.info(`Waiting ${ms} milliseconds ...`);
-
-    // core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    // await wait(parseInt(ms));
-    // core.info((new Date()).toTimeString());
-
-    // core.setOutput('time', new Date().toTimeString());
-    // const stackname = core.getInput('stackname');
-    const stackname = 'govyrl/shopify-fulfillment-pipeline'
+    const stackname = core.getInput('stackname');
 
     if(!stackname) {
         throw Error('stackname must be defined!')
