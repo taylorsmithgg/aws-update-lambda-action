86a9ea402e9d7bafed606bf28943ba5999aebf84
Taylor Smith <taylorsmith@Taylors-MacBook-Pro-2.local>
2023-01-12 10:59:05 -0800

    revert manual stackname

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const updateFunctions = require('./update-function');
 
 async function run() {
   try {
-    // const stackname = core.getInput('stackname');
-    const stackname = 'hcs-dev';
+    const stackname = core.getInput('stackname');
 
     if(!stackname) {
         throw Error('stackname must be defined!')
